fd74a47d6312d5fcb67c301214f5955e004debf5
ibrahim <yzcibrahim@gmail.com>
2022-04-24 09:17:56 +0300

    Add project files.

---
+++ Hafta07_01WordMaster/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Hafta07_01WordMaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
